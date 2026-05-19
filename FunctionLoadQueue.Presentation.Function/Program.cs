4703b5169fec727891575d3ebbca52926bda2a6e
Yeisson Ochoa <yeisson.ochoa72@gmail.com>
2026-05-19 15:29:22 -0500

    Borrar proyecto FunctionLoadQueue.Presentation que ya no aplica

--- FunctionLoadQueue.Presentation.Function/Program.cs
+++
@@ -1,7 +0,0 @@
-using FunctionLoadQueue.Presentation.Function;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
-
-var host = builder.Build();
-host.Run();
